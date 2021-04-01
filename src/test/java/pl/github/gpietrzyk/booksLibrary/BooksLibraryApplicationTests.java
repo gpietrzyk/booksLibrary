ade22e6ea9a4dad7a638270cf4446cc408029e68
Grzegorz Pietrzyk <grzegorzpietrzyk92@gmail.com>
2021-04-01 22:13:11 +0200

    Initial commit

---
+++ src/test/java/pl/github/gpietrzyk/booksLibrary/BooksLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.github.gpietrzyk.booksLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
